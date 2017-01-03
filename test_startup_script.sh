fecf82551ff10d2263e1bef6d9e422fabd9bbcb5
numair <numair.mansur@gmail.com>
2017-01-03 23:11:19 +0100

    changes

--- test_startup_script.sh
+++ test_startup_script.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-#redirect the output to the following file:
-#$ -o /home/mansurm/sgeout 
 
-
-#redirect the error messages to the following file:
-#$ -e /home/mansurm/sgeout
-
-echo HOME=$HOME
-echo USER=$USER
-echo JOB_ID=$JOB_ID
-echo JOB_NAME=$JOB_NAME
-echo HOSTNAME=$HOSTNAME
-echo SGE_TASK_ID=$SGE_TASK_ID
-
+echo "hello from job script!"
+echo "the date is" `date`
+echo "here's /etc/hosts contents:"
+echo "my name is numair"
+echo "finishing job :D"
