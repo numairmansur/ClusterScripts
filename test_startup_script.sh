58658d2d77e230622d3ceb07b9fe7c2b9ec545b9
Numair Mansur <numair.mansur@gmail.com>
2017-01-03 23:33:58 +0100

    Update test_startup_script.sh

--- test_startup_script.sh
+++ test_startup_script.sh
@@ -7,6 +7,12 @@
 #redirect the error messages to the following file:
 #$ -e /home/mansurm/output
 
+echo HOME=$HOME
+echo USER=$USER
+echo JOB_ID=$JOB_ID
+echo JOB_NAME=$JOB_NAME
+echo HOSTNAME=$HOSTNAME
+echo SGE_TASK_ID=$SGE_TASK_ID
 
 echo "hello from job script!"
 echo "the date is" `date`
