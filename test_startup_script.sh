6d8531ee63edf7ad24a810d38697c7d6ad527350
numair <numair.mansur@gmail.com>
2017-01-03 23:22:25 +0100

    changes

--- test_startup_script.sh
+++ test_startup_script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#redirect the output to the following file:
+#$ -o /home/kleinaa/output 
+
+
+#redirect the error messages to the following file:
+#$ -e /home/kleinaa/output
+
 
 echo "hello from job script!"
 echo "the date is" `date`
