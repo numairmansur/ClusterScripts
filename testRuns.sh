ec47126709910777435995d51876af6fc5eca634
Numair Mansur <numair.mansur@gmail.com>
2017-01-08 21:33:52 +0100

    Update testRuns.sh

--- testRuns.sh
+++ testRuns.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+#redirect the output to the following file:
+#$ -o /home/mansurm/output 
+
+
+#redirect the error messages to the following file:
+#$ -e /home/mansurm/output
+
+echo "running python script now:"
 python /home/mansurm/ClusterScripts/testPython.py
+echo "runnin successful"
