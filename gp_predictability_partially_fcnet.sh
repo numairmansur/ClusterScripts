d9dfdef6cf16d1795b6b7444211001e7577fac2c
numair <numair.mansur@gmail.com>
2017-01-22 21:31:28 +0100

    changes

--- gp_predictability_partially_fcnet.sh
+++
@@ -1,9 +0,0 @@
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=10
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=20
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=30
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=40
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=50
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=60
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=70
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=80
-python /ihome/kleinaa/devel/git/learning_curve_prediction/lc_prediction/experiments/predictability_partially.py with model_type=gp dataset=fcnet n_configs=4096 observed=90
