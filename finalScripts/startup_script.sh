581e406695f59a4208110c80151461261d78020a
numair <numair.mansur@gmail.com>
2017-01-23 11:18:19 +0100

    change

--- finalScripts/startup_script.sh
+++ finalScripts/startup_script.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-#redirect the output to the following file:
-#$ -o /home/mansurm/output 
-#redirect the error messages to the following file:
-#$ -e /home/mansurm/output
-echo HOME=$HOME
-echo USER=$USER
-echo JOB_ID=$JOB_ID
-echo JOB_NAME=$JOB_NAME
-echo HOSTNAME=$HOSTNAME
-echo SGE_TASK_ID=$SGE_TASK_ID
 source spearmintlite/bin/activate
 export PYTHONPATH='/home/mansurm/spearmint/spearmint/'
---
+++ finalScripts/testruns.sh
@@ -0,0 +1,1 @@
+python spearmint/spearmint/spearmint/main.py --driver=local --method=GPEIOptChooser --method-args=noiseless=1 Experiments/Bohachevsky/bohachevsky1/config.pb
