9ba47b87a365a1bf1bc6792d6779e85b273907d0
numair <numair.mansur@gmail.com>
2017-01-23 11:38:56 +0100

    changes

--- finalScripts/testruns.sh
+++ finalScripts/testruns.sh
@@ -1,1 +1,1 @@
-python spearmint/spearmint/spearmint/main.py --driver=local --method=GPEIOptChooser --method-args=noiseless=1 Experiments/Bohachevsky/bohachevsky1/config.pb
+python /home/mansurm/spearmint/spearmint/spearmint/main.py --driver=local --method=GPEIOptChooser --method-args=noiseless=1 /home/mansurm/Experiments/Bohachevsky/bohachevsky1/config.pb
